500ec6666869c82714b2d7e9ca28b89e4c64c31c
gaspar91 <andreiafg.91@gmail.com>
2020-06-23 12:43:26 +0000

    continue

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,36 +1,40 @@
-// The very first thing that we do is to create a new instance of the XMLHttpRequest object.
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-var xhr = new XMLHttpRequest(); 
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-// XML stands for Extensible Markup Language, 
-// which is similar to HTML in the way it structures its data, and it's a precursor to JSON.
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
 
-// Then we have the xhr.open() method, and the first argument that we parse in is "GET".
-// The second argument is the URL that we want to retrieve.
-// So, in this case, it's "https://ci-swapi.herokuapp.com/api/".
-// We're making a request to the Star Wars API.
+function getTableHeaders(obj) {
+    var tableHeaders = [];
 
-xhr.open("GET","https://ci-swapi.herokuapp.com/api/");
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
 
-// Now there are several different methods that we can use to communicate with a web server.
-// The two that you're going to use most often are GET and POST.
-// The GET method is used when we're retrieving data from the server.
-// This is a standard one that a browser users when retrieving a web page.
-// POST is used when we're sending data to the server, such as an uploaded file or form data.
-// Since in this instance we want to retrieve data from the Star Wars API, then we're going to use the GET method.
+    return `<tr>${tableHeaders}</tr>`;
+}
 
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
 
-// And then we do xhr.send() to send the request.
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
-xhr.send();
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
 
-// The main chunk of what's going on in this piece of code is in this onreadystatechange() function.
-// The xhr object maintains an internal state as it's completing various parts of our request operation.
-// And "readyState = 4" means that the operation has been completed.
-
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("data").innerHTML = this.responseText;
-  }
-};
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
